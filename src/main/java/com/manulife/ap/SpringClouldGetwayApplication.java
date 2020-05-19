216402b8db830c1926ca397f67c5d824c5a8eb1d
changqinrong <changqinrong1989@tom.com>
2020-05-19 22:33:55 +0800

    init project

---
+++ src/main/java/com/manulife/ap/SpringClouldGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.manulife.ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClouldGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClouldGetwayApplication.class, args);
+	}
+
+}
